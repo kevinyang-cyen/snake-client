9e19412ab3869dd66801454ed28b034fc1069b7d
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-23 13:41:03 -0700

    added movement functionality

--- input.js
+++ input.js
@@ -2,16 +2,28 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   const handleUserInput = stdin.on('data', (data) => {
-    if (data == '\u0003') {
+    if (data == 'w') {
+      conn.write('Move: up');
+    } else if (data == 's') {
+      conn.write('Move: down');
+    } else if (data == 'a') {
+      conn.write('Move: left');
+    } else if (data == 'd') {
+      conn.write('Move: right');
+    } else if (data == '\u0003') {
       process.exit();
     }
   });
+
   return stdin;
 }
 
