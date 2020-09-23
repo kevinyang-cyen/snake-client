017c5c72207701579d25d24541d4f594b46cd0ce
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-23 14:02:50 -0700

    changed the PORT and IP values to be constants and added a constants.js file

--- client.js
+++ client.js
@@ -1,13 +1,14 @@
 // client.js
 const net = require('net');
+const constants = require('./constants');
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    host: constants.IP,
+    port: constants.PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
