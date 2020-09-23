6ba423f9077462c55d2a6eecef57b1592c6fbd1b
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-23 13:20:35 -0700

    added quit functionality

--- play.js
+++ play.js
@@ -2,3 +2,27 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (data) => {
+    if (data == '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+}
+
+
+
+
+setupInput();
